384273d721bedec00dc887be659c2cb47556469c
premalatha06 <apremalatha62@gmail.com>
2026-07-02 18:40:33 +0530

    Initial Spring Boot scaffold via Initializr

---
+++ src/main/java/com/team7/carbontrack/CarbontrackApplication.java
@@ -0,0 +1,13 @@
+package com.team7.carbontrack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarbontrackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarbontrackApplication.class, args);
+	}
+
+}
